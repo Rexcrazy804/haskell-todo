762d05958ab04ec91c09285b69848a4b25dba03e
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-05 19:39:59 +0400

    nix: correct package derrivation

--- default.nix
+++ default.nix
@@ -6,15 +6,16 @@
   root = ./.;
 
   haskellFileFilter = fileFilter (file: builtins.any file.hasExt ["hs"]);
+  src = toSource {
+    inherit root;
+    fileset = unions [
+      (haskellFileFilter (root + /app))
+      (haskellFileFilter (root + /lib))
+      (haskellFileFilter (root + /test))
+      (root + /todo.cabal)
+      (root + /CHANGELOG.md)
+      (root + /UNLICENSE)
+    ];
+  };
 in
-  haskellPackages.developPackage {
-    root = toSource {
-      inherit root;
-      fileset = unions [
-        (haskellFileFilter (root + /app))
-        (haskellFileFilter (root + /lib))
-        (haskellFileFilter (root + /test))
-        (root + /todo.cabal)
-      ];
-    };
-  }
+  haskellPackages.callCabal2nix "todo" src {}
--- npins.nix
+++ npins.nix
@@ -4,7 +4,7 @@
 }:
 pkgs.lib.fix (self: let
   inherit (pkgs) haskellPackages;
-  inherit (pkgs.lib) attrValues;
+  inherit (pkgs.lib) attrValues map getAttr;
 in {
   packages = {
     todo = pkgs.callPackage ./default.nix {};
@@ -12,7 +12,7 @@ in {
   };
 
   devShells.default = pkgs.mkShell {
-    inputsFrom = [self.packages.default];
+    inputsFrom = map (getAttr "env") [self.packages.default];
     packages = attrValues {
       inherit (pkgs) haskell-language-server fourmolu;
       inherit (haskellPackages) cabal-fmt cabal-install;
